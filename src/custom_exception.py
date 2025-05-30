253ebf5e9fef14ced4d31155ece412c255144e5a
Chetan Tiwari <chetantiwaridgp5@gmail.com>
2025-05-30 16:34:20 +0530

    commit

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -13,13 +13,10 @@ class CustomException(Exception):
     @staticmethod
     def get_detailed_error_message(error_message, error_detail: sys):
         _, _, exc_tb = traceback.sys.exc_info()
-        if exc_tb is not None:
-            line_number = exc_tb.tb_lineno
-            file_name = exc_tb.tb_frame.f_code.co_filename
-            return f"Error occurred in script: [{file_name}] at line number: [{line_number}] with message: [{error_message}]"
-        else:
-            # No traceback available; just return the error message
-            return error_message
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+        return f"Error occurred in script: [{file_name}] at line number: [{line_number}] with message: [{error_message}]"
+  
 
     def __str__(self):
         return self.error_message
